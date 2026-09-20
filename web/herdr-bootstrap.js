6d8140fec67aa342f32a50827e55e7411877b2df
Lenin AGC <934933+IGUNUBLUE@users.noreply.github.com>
2026-09-20 12:39:43 -0500

    release: ship 0.25.0 web bundle

--- web/herdr-bootstrap.js
+++ web/herdr-bootstrap.js
@@ -1,4 +1,4 @@
-const e = new URL(window.__HERDR_ENTRY__ || "/builds/0.24.0-391-dc272cf388816026/index.html", location);
+const e = new URL(window.__HERDR_ENTRY__ || "/builds/0.25.0-392-3e465093b17fa2f2/index.html", location);
   e.search = location.search;
   e.hash = location.hash;
   location.replace(e);
