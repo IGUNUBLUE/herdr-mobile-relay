c0f14ef94232753d2e76ea2876bb1c26d1824ca3
Lenin AGC <934933+IGUNUBLUE@users.noreply.github.com>
2026-09-21 07:54:15 -0500

    Release 0.26.3

--- web/lerdr-bootstrap.js
+++ web/lerdr-bootstrap.js
@@ -1,4 +1,4 @@
-const e = new URL(window.__HERDR_ENTRY__ || "/builds/0.26.2-401-1022f191fd54aa82/index.html", location);
+const e = new URL(window.__HERDR_ENTRY__ || "/builds/0.26.3-402-e7e4fc0ddf82ac60/index.html", location);
   e.search = location.search;
   e.hash = location.hash;
   location.replace(e);
